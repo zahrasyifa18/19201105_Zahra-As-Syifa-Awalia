4c2c5daa590cba19f4465a2646cada6d0e0ad306
19201105_Zahra As Syifa Awalia <90739991+zahrasyifa18@users.noreply.github.com>
2021-10-08 14:45:06 +0700

    Tugas Pertemuan 4

---
+++ v_about.php
@@ -0,0 +1,21 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Zahra As</li> 
+        <li>Nama Belakang : Syifa Awalia</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Jalan Akmaliah 2, Depok</li> 
+    </ul> 
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Jakarta</li> </ul> 
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Bulutangkis</li> 
+        <li>Catur</li> 
+    </ul> 
+</section>
